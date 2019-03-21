b7ce24d134c1456ff11f15dc8980b61fee04cb7d
jiyeon <jiyeonk1227@gmail.com>
2019-03-21 16:48:08 +0900

    first

---
+++ src/main/java/com/bank/coconut/config/RootConfig.java
@@ -0,0 +1,44 @@
+package com.bank.coconut.config;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+@ComponentScan(basePackages = "com.bank.coconut" )
+public class RootConfig {
+
+	@Bean
+	public DataSource dataSource() {
+		HikariConfig hikariConfig = new HikariConfig();
+		hikariConfig.setDriverClassName("org.mariadb.jdbc.Driver");
+		hikariConfig.setJdbcUrl("jdbc:mariadb://coconutbank.cv6tnawqeoim.ap-northeast-2.rds.amazonaws.com/coconutbank");
+		hikariConfig.setUsername("coconutbank");
+		hikariConfig.setPassword("coco1234");
+
+		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+
+		return dataSource;
+	}
+
+	@Bean
+	public SqlSessionFactory sqlSessionFactory() throws Exception {
+		SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+		sqlSessionFactory.setDataSource(dataSource());
+		sqlSessionFactory
+				.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath*:mapper/**.xml"));
+		sqlSessionFactory.setTypeAliasesPackage("com.bank.coconut");
+		return (SqlSessionFactory) sqlSessionFactory.getObject();
+	}
+
+	
+
+}
---
+++ src/main/java/com/bank/coconut/config/WebConfig.java
@@ -0,0 +1,45 @@
+package com.bank.coconut.config;
+
+
+import javax.servlet.Filter;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.bank.coconut")
+@MapperScan(basePackages = "com.bank.coconut" )
+public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] {RootConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] {ServletConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String[] {"/"};
+	}
+	
+	
+
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+        encodingFilter.setEncoding("UTF-8");
+ 
+        return new Filter[]{encodingFilter};
+    }
+
+
+	
+}
